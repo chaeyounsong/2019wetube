68d3f379651ce119fa1a960b743ef963cc39a6e8
chaeyounsong <51305348+chaeyounsong@users.noreply.github.com>
2019-11-29 17:50:42 +0900

    started the server
    making req,res

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Hi!');
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+function handleListening(){
+    console.log(`Listening On : http://localhost:${PORT}`);
+}
+
+function handelHome(req,res){
+    
+    res.send('Hello From Home.')
+}
+
+function handelProfile(req,res){
+   
+    res.send('You are on my profile.');
+}
+
+app.get("/profile", handelProfile);
+app.get("/", handelHome);
+app.listen(PORT,handleListening);
+
